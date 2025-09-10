0cbe42fced643c50e93144e53264df6ca33111c2
DuskStarGITHUB <contactspeencer@gmail.com>
2025-09-10 15:38:03 -0600

    fixed size radio

--- src/app/modules/section1.tsx
+++ src/app/modules/section1.tsx
@@ -4,16 +4,18 @@ import MainImage from "./mainImage";
 
 const Section1 = () => {
   return (
-    <section className="relative min-h-screen w-full">
-      <div className="absolute inset-0 -z-0">
-        <AspectRatio ratio={16 / 9} className="w-full min-h-screen">
-          <Image
-            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=1600&dpr=2&q=80"
-            alt="Background"
-            fill
-            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </AspectRatio>
+    <section className="relative max-h-screen w-full">
+      <div className="absolute inset-0 -z-0 flex justify-center items-start overflow-hidden">
+        <div className="w-full">
+          <AspectRatio ratio={16 / 9} className="w-full h-full">
+            <Image
+              src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=1600&dpr=2&q=80"
+              alt="Background"
+              fill
+              className="object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </AspectRatio>
+        </div>
       </div>
       <div className="relative z-10 flex min-h-screen">
         <div className="w-1/2 flex items-center justify-center p-4 relative">
@@ -24,7 +26,7 @@ const Section1 = () => {
             <span className="font-bold">
               Vive La Gran Experiencia De La Fiesta
             </span>
-            , Encuentre las mejores Opciones para sus Eventos,Fiestas y
+            , Encuentre las mejores Opciones para sus Eventos, Fiestas y
             Reuniones...
           </p>
         </div>
