e066d447324da01702641f9fc982de50bf487f6d
DuskStarGITHUB <contactspeencer@gmail.com>
2025-09-11 13:08:15 -0600

    pexel fixed

--- src/app/modules/mainImage.tsx
+++ src/app/modules/mainImage.tsx
@@ -9,6 +9,41 @@ const MainImage = () => {
       <div className="absolute inset-0 bg-black/90 dark:bg-white/90 mask-[url(/photos/1.png)] mask-center mask-cover translate-x-3 opacity-50" />
       <div className="absolute inset-0 bg-black/90 dark:bg-white/90 mask-[url(/photos/3.png)] mask-center mask-cover opacity-20" />
       <div className="absolute inset-0 bg-black/90 dark:bg-white/90 mask-[url(/photos/3.png)] mask-center mask-cover translate-x-3 opacity-50" />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
+      <div
+        className="absolute inset-0 
+                bg-[url('/photos/3.png')] bg-center bg-cover
+                [filter:blur(12px)] opacity-70"
+      />
     </div>
   );
 };
