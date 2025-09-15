7b9d2e2f3fc61a06477ec881383f168d33ac9551
DuskStarGITHUB <contactspeencer@gmail.com>
2025-09-15 16:04:36 -0600

    new contact

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -5,6 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Calendar } from "@/components/ui/calendar";
+import { Video, Facebook, Instagram } from "lucide-react";
 import "../css/contact.css";
 
 export default function PageContacto() {
@@ -34,7 +35,7 @@ export default function PageContacto() {
       const textarea =
         e.currentTarget.querySelector<HTMLTextAreaElement>("#body");
       if (textarea)
-        textarea.value = `Fecha: ${dateString}\n\nMensaje:\n${textarea.value}`;
+        textarea.value = `Fecha solicitada: ${dateString}\n\nMensaje:\n${textarea.value}`;
     }
   };
 
@@ -42,6 +43,13 @@ export default function PageContacto() {
     <div className="px-4 md:px-16 pt-12 md:pt-20">
       <section className="scroll-animate opacity-0 py-12 text-center">
         <h1 className="text-3xl md:text-5xl font-bold mb-4">Contacto</h1>
+        <p className="max-w-3xl mx-auto text-lg md:text-xl leading-relaxed">
+          Rellena este formulario para enviar un correo directo al DJ Jesús. Por
+          favor asegúrate de que los datos sean correctos, especialmente tu
+          correo y la fecha que deseas consultar. Esta fecha no garantiza
+          reserva, solo sirve para preguntar disponibilidad. El DJ te responderá
+          directamente al correo proporcionado.
+        </p>
       </section>
       <section className="scroll-animate opacity-0 py-0 max-w-3xl mx-auto">
         <form
@@ -87,13 +95,13 @@ export default function PageContacto() {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
-            <Label>Reservar Fecha</Label>
+            <Label className="mt-2">Fecha solicitada</Label>
             {mounted && (
               <Calendar
                 mode="single"
                 selected={selectedDate}
                 onSelect={setSelectedDate}
-                className="mt-4 w-full"
+                className="mt-2 w-full"
               />
             )}
           </div>
@@ -120,6 +128,51 @@ export default function PageContacto() {
           />
         </form>
       </section>
+      <section className="scroll-animate opacity-0 py-6 text-center animate-fadeInUp">
+        <p className="max-w-3xl mx-auto text-lg md:text-xl leading-relaxed">
+          También puedes contactarnos a través de nuestras redes sociales,
+          estaremos encantados de responder tus dudas.
+        </p>
+        <div className="flex justify-center gap-3 mt-4 animate-fadeInUp">
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full hover:scale-110 transition h-9 w-9"
+            asChild
+          >
+            <a
+              href="https://www.tiktok.com/@jesuspulido875?is_from_webapp=1&sender_device=pc"
+              target="_blank"
+            >
+              <Video className="h-4 w-4" />
+            </a>
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full hover:scale-110 transition h-9 w-9"
+            asChild
+          >
+            <a href="https://www.facebook.com/dmx.jesus.pulido" target="_blank">
+              <Facebook className="h-4 w-4" />
+            </a>
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full hover:scale-110 transition h-9 w-9"
+            asChild
+          >
+            <a
+              href="https://www.instagram.com/djjesuspulidodmx/"
+              target="_blank"
+            >
+              <Instagram className="h-4 w-4" />
+            </a>
+          </Button>
+        </div>
+      </section>
+
       <p className="max-w-3xl mx-auto text-lg md:text-xl leading-relaxed text-center mt-8">
         Para cualquier información o consulta, por favor lea nuestros{" "}
         <strong>
